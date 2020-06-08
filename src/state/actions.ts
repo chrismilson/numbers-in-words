8a985e6406e737fb9354db4ed0f5676ebc9eba42
Chris Milson <chris.w.milson@gmail.com>
2020-06-08 14:53:26 +0900

    Create basic app state
    Done with redux.

--- src/state/actions.ts
+++ src/state/actions.ts
@@ -1,35 +1,6 @@
-import {
-  ADD_TODO,
-  TOGGLE_TODO,
-  SET_VISIBILITY_FILTER,
-  AddTodoAction,
-  ToggleTodoAction,
-  SetVisibilityFilterAction
-} from './types/actions'
-import { VisibilityFilter } from './types/state'
+import { ActionType, SetTargetAction } from './types/actions'
 
-let nextTodoId = 0
-
-export function addTodo(text: string): AddTodoAction {
-  const id = nextTodoId++
-  return {
-    type: ADD_TODO,
-    payload: { text, id }
-  }
-}
-
-export function toggleTodo(id: number): ToggleTodoAction {
-  return {
-    type: TOGGLE_TODO,
-    payload: { id }
-  }
-}
-
-export function setVisibilityFilter(
-  filter: VisibilityFilter
-): SetVisibilityFilterAction {
-  return {
-    type: SET_VISIBILITY_FILTER,
-    payload: { filter }
-  }
-}
+export const setTarget = (value: number): SetTargetAction => ({
+  type: ActionType.SET_TARGET,
+  payload: { value }
+})
