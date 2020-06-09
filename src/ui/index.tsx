2388a6f11df6ff67e4bd4aaa518c3ddab94f7033
Chris Milson <chris.w.milson@gmail.com>
2020-06-09 14:22:28 +0900

    Link to the gist

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -6,7 +6,9 @@ import './App.css'
 export default function App() {
   return (
     <div className="App">
-      <h1>Numbers In Words</h1>
+      <a href="https://gist.github.com/chrismilson/48840cc4b77a56ee1caa9196ae1cc649">
+        <h1>Numbers In Words</h1>
+      </a>
       <InputDigits />
       <DisplayWords />
     </div>
