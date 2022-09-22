01f4aee29b6aeb68b971492a99cd86df204ffb0d
Chris Milson-Tokunaga <chris.w.milson@gmail.com>
2022-09-22 09:05:17 +0000

    Output values by language

---
+++ src/itoa/index.ts
@@ -0,0 +1,12 @@
+import * as japanese from './japanese'
+import * as english from './english'
+
+export const MAX_SAFE_NUMBER = {
+  japanese: japanese.MAX_SAFE_NUMBER,
+  english: english.MAX_SAFE_NUMBER
+}
+
+export const itoa = {
+  japanese: japanese.default,
+  english: english.default
+}
