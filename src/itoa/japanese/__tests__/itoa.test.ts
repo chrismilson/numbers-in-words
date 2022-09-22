92d93908aee118c001f2323dca3284cf7050d849
Chris Milson-Tokunaga <chris.w.milson@gmail.com>
2022-09-22 08:54:30 +0000

    Update tests

--- src/itoa/japanese/__tests__/itoa.test.ts
+++ src/itoa/japanese/__tests__/itoa.test.ts
@@ -1,15 +1,19 @@
 import itoa from '..'
 
 it.each([
-  [1, 'one'],
-  [10, 'ten'],
-  [13, 'thirteen'],
-  [101, 'one hundred and one'],
-  [1000000, 'one million'],
+  [1, 'いち'],
+  [10, 'じゅう'],
+  [13, 'じゅうさん'],
+  [101, 'ひゃくいち'],
+  [1000000, 'ひゃくまん'],
   [
     398623123986,
-    'three hundred and ninety-eight billion six hundred and twenty-three million one hundred and twenty-three thousand nine hundred and eighty-six'
-  ]
+    'さんぜん きゅうひゃく はちじゅう ろくおく にせん さんびゃく じゅう にまん さんぜん きゅうひゃく はちじゅう ろく'
+  ],
+  [800, 'はっぴゃく'],
+  [300, 'さんびゃく'],
+  [8000, 'はっせん'],
+  [3000, 'さんぜん']
 ])('Gives the right string for each number', (number, words) => {
   expect(itoa(BigInt(number))).toBe(words)
 })
